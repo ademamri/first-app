c216a58dbaf85a25f6ee4c558c0e93edeac311f5
ademamri <ademamri1608@gmail.com>
2024-07-11 17:27:02 +0100

    first

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+     <div >
+        <div className='Nav'>
+        <h1 className='tag'>hello word</h1>
+     <ul className='tag'>
+       <li className='tag'>home</li> 
+       <li className='tag'> about </li>
+     </ul></div>
+     <div className='react'>
+        <h1 className='test'>go my code</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdHUU7kX88c7M5R9lZsHZv0qyezS52tobkWQ&s" alt="" />
+     </div>
+    </div>
+
+
+  )
+}
+
+export default Navbar
